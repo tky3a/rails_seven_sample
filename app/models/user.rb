30238f81f5775c27836e7517595f61095cbaee47
tky <takayamac@icloud.com>
2022-02-18 14:30:35 +0900

    一旦通常のログイン機能だけにする

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable, :timeoutable, :trackable, :omniauthable
+         :recoverable, :rememberable, :validatable
+  #  :confirmable, :timeoutable, :trackable, :omniauthable
   validates :uid, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   has_many :articles

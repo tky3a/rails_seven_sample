115497da4b77e3268ec700c3df696cae96b4ae4d
tky <takayamac@icloud.com>
2022-02-19 21:40:36 +0900

    マイページにとべるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
+  #---------------- divise routiong start -------------------------------
   # これで認証系のルーティングが作成される
   devise_for :users
+  # registrationsとsessionsのルーティングのパスを修正(エラーでなければそのままでよさそう。。。)
   devise_scope :user do
     get "/users/new", to: "devise/registrations#new"
     get "/users/sign_out" => "devise/sessions#destroy"
   end
+  #---------------- divise routiong end -------------------------------
 
+  #---------------- user routiong start -------------------------------
+  get "/:uid", to: "users#show", as: :user
+  #---------------- user routiong end -------------------------------
+
+  #---------------- article routiong start -------------------------------
   root to: "articles#index"
   resources :articles, only: [:index, :new, :create] do
     collection do
@@ -21,12 +29,7 @@ Rails.application.routes.draw do
   get "/:uid/articles/:id", to: "articles#show", as: :article
   get "/:uid/articles/edit/:id", to: "articles#edit", as: :edit_article
   patch "/:uid/articles/edit/:id", to: "articles#update", as: :update_article
-  # resources :users
-  # --------------
-  # resources :users, only: [:index, :show, :new, :create] do
-  #   resources :articles, only: [:show]
-  # end
-  # --------------
+  #---------------- article routiong end -------------------------------
 
   # namespace "/:user_uid", param: :uid do
   #   resources :articles, only: [:show]
